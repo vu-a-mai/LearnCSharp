1000cbd70a878b1b192fd227d769006fd7c52872
Vu Mai <vuanhmai.io@gmail.com>
2024-03-05 02:02:10 -0800

    Boolean Data Type

---
+++ BooleanDataType/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BooleanDataType
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // A boolean can only hold True or False value
+            // Declare boolean variable and intialize it
+            bool value = true;
+            bool isMale = true;
+            Console.WriteLine(isMale);
+
+            // assigned boolean variable to new state
+            isMale = false;
+            Console.WriteLine(isMale);
+
+            Console.ReadLine();
+        }
+    }
+}
