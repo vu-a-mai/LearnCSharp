e5901c7a18a8179442100200af8ba3375fedd05a
Vu Mai <vuanhmai.io@gmail.com>
2024-03-05 01:49:33 -0800

    Text Based Data Types

---
+++ TextBasedDataTypes/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TextBasedDataTypes
+{
+    internal class String_Chars
+    {
+        static void Main(string[] args)
+        {
+            // Declare a string variable name
+            String name = "John";
+            // Declare a char variable letter
+            char letter = 'a';
+
+            // Print without a new line
+            Console.Write("Your name is ");
+            Console.Write(name);
+
+            // Print with a new line
+            Console.WriteLine();
+            Console.WriteLine(letter);   
+
+            Console.ReadLine();
+        }
+    }
+}
