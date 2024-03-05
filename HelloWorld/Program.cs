cb7c0ecab40ff25bf7b18d5fe165eb0c16e9c83e
Vu Mai <vuanhmai.io@gmail.com>
2024-03-04 21:35:23 -0800

    First "Hello World!" console program

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -10,6 +10,12 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
+            // Console.Writeline is a reference belong to "System" library in line 1
+            // Purpose: printing "Hello World!" in the console application.
+            Console.WriteLine("Hello World!");
+
+            // Console waiting for user input and press enter before continue...
+            Console.ReadLine();
         }
     }
 }
