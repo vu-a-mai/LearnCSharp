5128328fcde620b1eec85fb0db955f334a701b5d
Vu Mai <vuanhmai.io@gmail.com>
2024-03-05 22:05:41 -0800

    Switch statements

---
+++ Switch_Statements/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Switch_Statements
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a day of the week: ");
+            int day = Convert.ToInt32(Console.ReadLine());
+
+            // Instead of doing if/else statements
+            /*
+            if (day == 1) 
+            {
+                Console.WriteLine("Monday");
+            }
+            else if (day == 2)
+            {
+                Console.WriteLine("Tuesday");
+            }
+            else
+            {
+                Console.WriteLine("Enter the value from 1 - 7.");
+            }
+            */
+
+            switch (day)
+            {
+                case 1: 
+                    Console.WriteLine("Monday");
+                    break;
+                case 2: 
+                    Console.WriteLine("Tuesday");
+                    break;
+                case 3:
+                    Console.WriteLine("Wednesday");
+                    break;
+                case 4:
+                    Console.WriteLine("Thursday");
+                    break;
+                case 5:
+                    Console.WriteLine("Friday");
+                    break;
+                case 6:
+                    Console.WriteLine("Saturday");
+                    break;
+                case 7:
+                    Console.WriteLine("Sunday");
+                    break;
+                default:
+                    Console.WriteLine("Invalid, enter a valid between 1 and 7.");
+                    break;
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
