4ab7d169f1d258dc10f27b493f49d10b11fe1506
Vu Mai <vuanhmai.io@gmail.com>
2024-03-06 16:03:03 -0800

    String Interpolation Example

---
+++ String Interpolation/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace String_Interpolation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "John";
+            int age = 25;
+
+            Console.WriteLine("Your name is " + name + " your age is " + age);
+
+            // Using $ to interpolate
+            // insert $ before the string and variable inside {}
+            Console.WriteLine($"Your name is {name} your age is {age}");
+
+            Console.ReadLine();
+        }
+    }
+}
