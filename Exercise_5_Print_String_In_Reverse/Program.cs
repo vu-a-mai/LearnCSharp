26a9d399bd05c0fbc4a4e1d9b22ea2850c92fe9e
Vu Mai <vuanhmai.io@gmail.com>
2024-03-06 16:55:36 -0800

    Exercise 5 Printing String In Reverse order

---
+++ Exercise_5_Print_String_In_Reverse/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise_5_Print_String_In_Reverse
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             * Ask user to input message
+             * Print in order
+             * Print in reverse
+             */
+            Console.Write("Enter your message: ");
+            string message = Console.ReadLine();
+
+            //Console.WriteLine(message[0]);
+            //Console.WriteLine(message.Length);
+
+            Console.WriteLine("Printing in order");
+            // Print in order
+            for (int i = 0; i < message.Length; i++)
+            {
+                Console.Write(message[i]);
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Printing in reverse");
+            // Print in reverse
+            for (int i = message.Length - 1; i >= 0; i--)
+            {
+                Console.Write(message[i]);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
