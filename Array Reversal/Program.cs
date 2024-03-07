4c00309ab7b1d8e91c9b28ea0b111d634fd69c98
Vu Mai <vuanhmai.io@gmail.com>
2024-03-06 20:44:44 -0800

    Array built in Reverse Function Example

---
+++ Array Reversal/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Array_Reversal
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = new int[] 
+            { 
+                1, 2, 3, 4 ,5 
+            };
+
+            // Array Reverse function
+            Array.Reverse(numbers);
+
+            foreach (var number in numbers) 
+            {
+                Console.Write($"{number} ");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
