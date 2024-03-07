c6f881170636b74ad7f67583266464ff26c0aa5d
Vu Mai <vuanhmai.io@gmail.com>
2024-03-06 20:59:25 -0800

    Array clearing example using the build in Array.Clear  function or using for loop.

---
+++ Array Clearing/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Array_Clearing
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = new int[]
+            {
+                0, 1, 2, 3, 4, 5, 6, 7, 8, 9
+            };
+
+            // Array.Clear will assigned all the position to the value of 0
+            //Array.Clear(numbers, 0, numbers.Length);
+            Array.Clear(numbers, 5, 5); // start at 5th position and go 5 forward
+
+            /*
+            // Using for loop to assigned to default
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                // numbers[i] = 0;
+                // use default value, so you don't have to remember the data type
+                // easier and safer option
+                numbers[i] = default;
+            }
+
+            // Using for loop to reassigned starting from index 5 to 10 
+            for (int i = 5; i < 10; i++) // 5 6 7 8 9
+            {
+                numbers[i] = default;
+            }
+            */
+
+            foreach (var item in numbers)
+            {
+                Console.Write($"{item} ");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
