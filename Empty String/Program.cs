7db68667fd07b37c2c32659556e6b4d284cb2607
Vu Mai <vuanhmai.io@gmail.com>
2024-03-06 16:12:58 -0800

    Empty String Example

---
+++ Empty String/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Empty_String
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter your name: ");
+            string name = Console.ReadLine();
+
+            // Using string.Empty to help code maintainable and readable instead of ""
+            if (name != string.Empty) //""
+            { 
+                Console.WriteLine($"Your name is {name}");
+            }
+            else
+            {
+                Console.WriteLine("Name is empty");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
