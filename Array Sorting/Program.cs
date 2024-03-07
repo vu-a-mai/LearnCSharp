88b2b968c168ecdb6c5216e414e8ba8414a7121b
Vu Mai <vuanhmai.io@gmail.com>
2024-03-06 20:41:47 -0800

    Array sorting function Example

---
+++ Array Sorting/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Array_Sorting
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = new int[]
+            {
+                //1, 2, 3, 4, 5, 6, 7, 8, 9
+                9, 2, 0 , 8, 1, 5, 6, 4, 3, 2
+            };
+
+            string test = "";
+            test.Replace(" ", "");
+
+            // Array Sort functions
+            Array.Sort(numbers);
+
+            foreach (int number in numbers)
+            {
+                Console.Write($"{number} ");
+            }            
+            
+            Console.ReadLine();
+        }
+    }
+}
