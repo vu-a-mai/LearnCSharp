3472d8d3cec0d48bc5c9176e47ea74e1143ae787
Vu Mai <vuanhmai.io@gmail.com>
2024-03-07 20:50:30 -0800

    Void Functions

---
+++ Void Functions/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Void_Functions
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Welcome function
+            WelcomeMessage();
+
+            // Create and Print Array function
+            CreateAndPrintArray();
+
+            Console.ReadLine();
+        }
+
+        static void CreateAndPrintArray()
+        {
+            int[] numbers = new int[5] { 0, 1, 2, 3, 4 };
+
+            foreach (var item in numbers)
+            {
+                Console.Write($"{item} ");
+            }
+        }
+
+        static void WelcomeMessage()
+        {
+            Console.WriteLine("Welcome to my program!");
+        }
+    }
+}
