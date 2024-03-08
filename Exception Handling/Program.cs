57d5a44d81a4623cdec4991df4c78967956008a2
Vu Mai <vuanhmai.io@gmail.com>
2024-03-08 03:02:53 -0800

    Exception Handling Example

---
+++ Exception Handling/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exception_Handling
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a number: ");
+            int num = Convert.ToInt32(Console.ReadLine());
+
+            // Will get overflow, if user enter number bigger than the max Int32.
+            // Have to manually handle the exception, otherwise the program crash
+            Console.WriteLine(num);
+
+            Console.ReadLine();
+        }
+    }
+}
