875189adda45b3a7bdacf34041b1fe23effcf223
Vu Mai <vuanhmai.io@gmail.com>
2024-03-08 02:45:05 -0800

    Exercise 9: Area of a Triangle

---
+++ Exercise_9_Area_of_Triangle/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise_9_Area_of_Triangle
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int width = ReadInt("width");
+            int height = ReadInt("height");
+
+            int result = CalculateAreaofTriangle(width, height);
+
+            Console.WriteLine($"The area is {result}");
+
+            Console.ReadLine();
+            
+        }
+
+        static int CalculateAreaofTriangle(int width, int height)
+        {
+            return (width * height) / 2;
+        }
+
+        static int ReadInt(string message)
+        {
+            Console.Write($"Enter {message}: ");
+            return Convert.ToInt32(Console.ReadLine());
+        }
+    }
+}
