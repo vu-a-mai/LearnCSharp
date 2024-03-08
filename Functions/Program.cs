1a873af70411ba9231b4cbac9a80865c33b3ceac
Vu Mai <vuanhmai.io@gmail.com>
2024-03-07 20:43:01 -0800

    Functions Examples

---
+++ Functions/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Functions
+{
+    class Test
+    {
+        public static void someFunction()
+        {
+            Console.WriteLine("Some function");
+        }
+
+        public void differentFunction()
+        {
+            Console.WriteLine("Different function");
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Call the function
+            WelcomeMessage();
+
+            // To use the function from class test
+            // we can use it directly if it is static
+            Test.someFunction();
+
+            // If the function is not static
+            // make the new instant of the class
+            Test newTest = new Test();
+            newTest.differentFunction();
+            
+            Console.ReadLine();
+        }
+
+        // Seperate function from the main function
+        // Use static, we don't to create new instant
+        static void WelcomeMessage()
+        {
+            Console.WriteLine("Welcome John");
+        }
+    }
+}
