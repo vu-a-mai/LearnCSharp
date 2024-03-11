dca8173269b3eb0430f1759d6a0f78616938740b
Vu Mai <vuanhmai.io@gmail.com>
2024-03-11 02:35:32 -0700

    Exercise_12_Fix_Logic_Error using Watch Window Example to check values

---
+++ Exercise_12_Fix_Logic_Error/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise_12_Fix_Logic_Error
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int age = 51;
+
+            if (age > 18 && age < 30)
+            {
+                Console.WriteLine("Over 18");
+            }            
+            else if (age > 30 && age <= 50)
+            {
+                Console.WriteLine("Over 30");
+            }
+            else if (age > 50)
+            {
+                Console.WriteLine("Over 50");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
