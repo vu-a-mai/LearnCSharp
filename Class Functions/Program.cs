ef888cd492bfaf8a32734a26b7c6ce4f7ca4051e
Vu Mai <vuanhmai.io@gmail.com>
2024-03-11 15:31:32 -0700

    Class Function Example

---
+++ Class Functions/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class_Functions
+{
+    internal class Program
+    {
+        class Person
+        {
+            public string name;
+            public int age;
+
+            public Person(string name, int age)
+            {
+                this.name = name;
+                this.age = age;
+            }
+
+            // Function inside the class
+            public string ReturnDetails()
+            {
+                return $"Name: {name}\nAge: {age}";
+            }
+        }
+        static void Main(string[] args)
+        {
+
+            Person person = new Person("John", 25);
+
+            //Console.WriteLine(ReturnDetails(person.name, person.age));
+            
+            // Pass in the class
+            Console.WriteLine(ReturnDetails(person));
+
+            // Calling from class
+            Console.WriteLine(person.ReturnDetails());
+
+            Console.ReadLine();
+        }
+
+        // Pass in Person
+        static string ReturnDetails(Person person)
+        {
+            return $"Name: {person.name}\nAge: {person.age}";
+        }        
+        /*
+        static string ReturnDetails(string name, int age)
+        {
+            return $"Name: {name}\nAge: {age}";
+        }
+        */
+    }
+}
---
+++ Class Functions/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Class Functions")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Class Functions")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("45ada14f-1f11-4ac0-b392-b2fa77b256fe")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
