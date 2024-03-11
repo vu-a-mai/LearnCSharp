ad3791200e0fbadb4a4df35bcb0a7b1bf8c48889
Vu Mai <vuanhmai.io@gmail.com>
2024-03-11 01:39:43 -0700

    Printing Error Messages Examples

---
+++ Printing Error Messages/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Printing_Error_Messages
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.Write("Enter a number: ");
+                int num = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine(num);
+            }
+            catch (FormatException e)
+            {
+                Console.WriteLine($"Error: {e.Message}");
+                Console.WriteLine("Please only enter numbers!");
+            }
+            catch (OverflowException e)
+            {
+                Console.WriteLine($"Error: {e.Message}");
+                Console.WriteLine("Please enter a value under 2 billion!");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"Error: {e.Message}");
+            }
+
+            Console.WriteLine("Goodbye!");
+
+            Console.ReadLine();
+        }
+    }
+}
