6fe2057da56d055e050a655f800fb46d81c7d37b
Vu Mai <vuanhmai.io@gmail.com>
2024-03-11 02:16:19 -0700

    Debugging and Check Auto/Local Windows Example

---
+++ Local_Auto_Window/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Local_Auto_Window
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "John";
+            int age;    // 0
+            age = 25;
+
+            PrintDetails(name, age);
+
+            string message = ReadFromConsole("Enter something: ");
+            Console.WriteLine(message);
+
+            Console.ReadLine();
+        }
+
+        static string ReadFromConsole(string message)
+        {
+            Console.Write(message);
+            return Console.ReadLine();
+        }
+        static void PrintDetails(string name, int age)
+        {
+            Console.WriteLine($"{name} - {age}");
+        }
+    
+    }
+}
