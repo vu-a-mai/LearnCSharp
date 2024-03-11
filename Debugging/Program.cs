d1ad9db8b22ab2f80590fb5fcfed7524c55a5743
Vu Mai <vuanhmai.io@gmail.com>
2024-03-11 02:10:44 -0700

    Debugging Examples

---
+++ Debugging/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Debugging
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "John";
+            int age;    // 0
+            age = 25;
+
+            PrintDetails(name, age);
+
+            string message = ReadFromConsole("Enter something: ");
+            Console.WriteLine(message);
+
+            Console.ReadLine();
+        }
+
+        static string ReadFromConsole(string message)
+        {
+            Console.Write(message);
+            return Console.ReadLine();
+        }
+        static void PrintDetails(string name, int age)
+        {
+            Console.WriteLine($"{name} - {age}");
+        }
+    }
+}
